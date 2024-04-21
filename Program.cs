607989e0912a382b265106506722b495e1f01fd1
MertBasar0 <mertbasar0@hotmail.com>
2024-04-22 02:13:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using gRPCExample;
+
+GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7289", new GrpcChannelOptions() { MaxReceiveMessageSize = 2000000000});
+
+var client = new ExampleRPCServices.ExampleRPCServicesClient(channel);
+
+#region bi
+
+#endregion
+#region ClientStream
+//var request = new exampleRequest()
+//{
+//Name = "Aysun",
+//Surname = "Başar"
+//};
+
+
+//var clientStream = client.ClientStreamMethod();
+//for (int i = 0; i < 30; i++)
+//{
+//await clientStream.RequestStream.WriteAsync(request);
+
+//}
+//await clientStream.RequestStream.CompleteAsync();
+//var response = await clientStream.ResponseAsync;
+//Console.WriteLine(response.ToString());
+
+
+#endregion
+#region ServerStream
+//var triggerMessage = new emptyMessageForTrigger();
+
+//var res = client.ServerStreamMethod(request);
+
+//int index = 0;
+
+//await foreach (var item in res.ResponseStream.ReadAllAsync())
+//{
+//    index++;
+//    Console.WriteLine($"{item}\nişlem bitti\n\n\n\n\n{index}");
+//}
+
+//var res = await client.UnaryTriggerAsync(triggerMessage);
+
+//Console.WriteLine($"{res}\nişlem bitti");
+#endregion
+
+Console.ReadLine();
+
